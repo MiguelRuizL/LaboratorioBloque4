eb1675fed3c511bf98c0cc7b0e37cdfdd5a3f8e8
MiguelRuizL <miguel_an85@hotmail.com>
2026-05-21 19:13:10 -0600

    Proyecto base creado con Spring Initialzr. Usa Spring Boot.

---
+++ src/main/java/com/lab4/axity/AxityApplication.java
@@ -0,0 +1,13 @@
+package com.lab4.axity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxityApplication.class, args);
+	}
+
+}
